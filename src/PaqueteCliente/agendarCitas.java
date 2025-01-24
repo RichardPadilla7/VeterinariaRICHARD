5cb6069ef6c8b2a9ab5cb24817d98a8b9eb17535
RichardPadilla7 <richardpadilla3rod@gmail.com>
2025-01-24 12:54:22 -0500

    AGEnDAR CITAS - RICHARD PADILLA

---
+++ src/PaqueteCliente/agendarCitas.java
@@ -0,0 +1,144 @@
+package PaqueteCliente;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import PaqueteRecursos.conexion;
+
+public class agendarCitas extends conexion{
+    public JPanel PAgendar;
+    public JTextField textField1;
+    public JTextField textField2;
+    public JTextField textField3;
+    public JTextField textField8;
+    public JTextField textField9;
+    public JButton agendarCitaButton;
+    public JButton regresarButton;
+    public JTextArea textArea1;
+    public JCheckBox chequeoGeneralCheckBox;
+    public JCheckBox vacunacionCheckBox;
+    public JCheckBox cirugiaCheckBox;
+    public JCheckBox aseoCheckBox;
+    public JCheckBox machoCheckBox;
+    public JCheckBox hembraCheckBox;
+    private JButton subirFotoButton;
+    private JLabel fotoLabel;
+
+    public agendarCitas() {
+
+        String cedulaCLiente = textField1.getText();
+        String tipoMascota = textField2.getText();
+        String nombreMascota = textField3.getText();
+        String sexoMascota = "";
+        String tipoServicio = "";
+        String motivaCita = textField8.getText();
+        double costo = textField9.getColumns();
+        String observaciones = textArea1.getText();
+
+
+
+
+        ButtonGroup grupoServicio = new ButtonGroup();
+        grupoServicio.add(cirugiaCheckBox);
+        grupoServicio.add(aseoCheckBox);
+        grupoServicio.add(chequeoGeneralCheckBox);
+        grupoServicio.add(vacunacionCheckBox);
+
+        ButtonGroup grupoGenero = new ButtonGroup();
+        grupoGenero.add(machoCheckBox);
+        grupoGenero.add(hembraCheckBox);
+
+
+        agendarCitaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+
+                String servicio = "";
+
+                if (cirugiaCheckBox.isSelected()) {
+                    servicio = "cirugia";
+                } else if (aseoCheckBox.isSelected()) {
+                    servicio = "aseo";
+                } else if (chequeoGeneralCheckBox.isSelected()) {
+                    servicio = "chequeo genero";
+                } else if (vacunacionCheckBox.isSelected()) {
+                    servicio = "vacunacion";
+                }
+
+                String genero = "";
+
+                if (machoCheckBox.isSelected()) {
+                    genero = "macho";
+                } else if (hembraCheckBox.isSelected()) {
+                    genero = "hembra";
+                }
+
+                if (servicio.isEmpty() || genero.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Por favor, complete todos los campos");
+                    return;
+                }
+
+                try (Connection conn = connect()) {
+                    String sql = "INSERT INTO agendar_citas (usuario, contrasenia, rol) VALUES (?, ?, ?)";
+                    PreparedStatement pstmt = conn.prepareStatement(sql);
+                    pstmt.setString(1, cedulaCLiente);
+                    pstmt.setString(2, tipoMascota);
+                    pstmt.setString(3, nombreMascota);
+                    pstmt.setString(4, sexoMascota);
+                    pstmt.setString(7, tipoServicio);
+                    pstmt.setString(8, motivaCita);
+                    pstmt.setDouble(9, costo);
+                    pstmt.setString(10, observaciones);
+
+
+                    pstmt.execute();
+                    JOptionPane.showMessageDialog(null, "Agenda Registrada!");
+
+                    textField1.setText("");
+                    textField2.setText("");
+                    textField3.setText("");
+                    textField8.setText("");
+                    textField9.setText("");
+                    textArea1.setText("");
+                    grupoGenero.clearSelection();
+                    grupoServicio.clearSelection();
+
+
+                } catch (SQLException exception) {
+                    throw new RuntimeException(exception);
+                }
+
+            }
+        });
+
+        regresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new cliente();
+            }
+        });
+
+        subirFotoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                    JFileChooser fileChooser = new JFileChooser();
+                    fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                    int result = fileChooser.showOpenDialog(null);
+
+                    if (result == JFileChooser.APPROVE_OPTION) {
+                        File selectedFile = fileChooser.getSelectedFile();
+                        ImageIcon imageIcon = new ImageIcon(selectedFile.getAbsolutePath());
+                        Image image = imageIcon.getImage().getScaledInstance(180, 180, Image.SCALE_SMOOTH);
+                        fotoLabel.setIcon(new ImageIcon(image));
+                    }
+                }
+        });
+    }
+}
--- src/PaqueteCliente/cliente.java
+++ src/PaqueteCliente/cliente.java
@@ -1,10 +1,84 @@
 package PaqueteCliente;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
 
 public class cliente {
     public JPanel PCliente;
-    private JButton agendarCitaButton;
-    private JButton historialMédicoDeMiButton;
-    private JButton salirButton;
+    public JButton agendarCitaButton;
+    public JButton historialMédicoDeMiButton;
+    public JButton salirButton;
+
+    public cliente() {
+
+        JFrame frame = new JFrame("Cliente");
+        frame.setContentPane(PCliente);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 300);
+        frame.setPreferredSize(new Dimension(900, 500));
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+
+
+        agendarCitaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                JFrame frame = new JFrame("Gestion De Calificaciones");
+                frame.setContentPane(new agendarCitas().PAgendar);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(500, 300);
+                frame.setPreferredSize(new Dimension(900, 500));
+                frame.setLocationRelativeTo(null);
+                frame.pack();
+                frame.setVisible(true);
+
+
+            }
+        });
+
+
+        historialMédicoDeMiButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                JFrame frame = new JFrame("Gestion De Calificaciones");
+                frame.setContentPane(new agendarCitas().PAgendar);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(500, 300);
+                frame.setPreferredSize(new Dimension(900, 500));
+                frame.setLocationRelativeTo(null);
+                frame.pack();
+                frame.setVisible(true);
+
+
+
+            }
+        });
+
+
+        salirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                JFrame frame = new JFrame("Gestion De Calificaciones");
+                frame.setContentPane(new agendarCitas().PAgendar);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(500, 300);
+                frame.setPreferredSize(new Dimension(900, 500));
+                frame.setLocationRelativeTo(null);
+                frame.pack();
+                frame.setVisible(true);
+
+                // Cerrar la ventana de login actual
+                JFrame loginFrame = (JFrame) SwingUtilities.getWindowAncestor(PCliente);
+                loginFrame.dispose();
+
+
+            }
+        });
+    }
 }
